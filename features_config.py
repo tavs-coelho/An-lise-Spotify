3b8584880b5e2d87cba4469af8888c5a7b024892
Gustavo Coelho de Melo <116911674+tavs-coelho@users.noreply.github.com>
2025-12-12 17:33:06 -0300

    Add features configuration for preprocessing

---
+++ features_config.py
@@ -0,0 +1,20 @@
+# Lista de features numéricas para StandardScaler
+NUM_FEATURES = [
+    'danceability',
+    'energy',
+    'loudness',
+    'speechiness',
+    'acousticness',
+    'instrumentalness',
+    'liveness',
+    'valence',
+    'tempo',
+    'duration_ms'
+]
+
+# Lista de features categóricas para OneHotEncoder
+CAT_FEATURES = [
+    'key',
+    'mode',
+    'time_signature'
+]
